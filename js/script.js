f7071b757bcf8ff9755ae3ef1541f98115f0b1eb
Alexander <sashok935@gmail.com>
2021-07-01 01:11:34 +0300

    Initial upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const enButton = document.getElementById('en-btn');
+const jaButton = document.getElementById('ja-btn');
+const i18n = domI18n({
+  languages: ['en', 'ja'],
+});
+document.body.style.display = 'block';
+
+enButton.onclick = function () {
+  i18n.changeLanguage('en');
+};
+
+jaButton.onclick = function () {
+  i18n.changeLanguage('ja');
+};
